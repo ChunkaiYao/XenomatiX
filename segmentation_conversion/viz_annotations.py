ce3d723feec2849a25621a2b7765574ff801a5d2
Elton Lin <eltonlin@umich.edu>
2021-12-09 22:09:58 -0500

    Tidy up viz_annotations for easier use

--- segmentation_conversion/viz_annotations.py
+++ segmentation_conversion/viz_annotations.py
@@ -1,50 +1,82 @@
-# Tool to visualize 3d bbox in pointcloud (along with 2d image?) for verification and sanity check
+# Tool to visualize 3d bbox in pointcloud (could potentially display it along with corresponding 2d image for sanity check)
 import open3d as o3d
+from matplotlib import cm
 import numpy as np
+
+import argparse
 import os
-import sys
 
-# Example arguments:
-# out_fd = "/home/mcity/xenomatix/bbox_conversion/out2/"
-# frame_id = "frame_00005404"
+# README.md:
+# Valid examplex: 
+# ``python viz_annotations.py </abs/path/to/folder/frame_0000xxxx/> -b``
+# ``python viz_annotations.py </abs/path/to/folder/frame_0000xxxx/>``
 
-if len(sys.argv) != 3:
-    print("usage: python viz_annotations.py <path/to/folder/> <frame_0000xxxx>")
-    exit(0)
- 
-out_fd = sys.argv[1]
-frame_id = sys.argv[2]
+# ** The expected result is a single frame in open3d viewer that has pointcloud of annotated/segmented pedestrians. 
+# ** The -b flag is optional, which draws a bounding box around the pedestrians
+# ** To view at right orientation, orient the blue axis to point up, green to the left, and red into the screen.
+# (pressing down ctrl or shift will help rotate or lateral move, scroll to zoom in/out)
 
-ann_path = out_fd + frame_id + "/Annotations/"
+INTENSITY_MAX = 17000000
+COLOR_RANGE = INTENSITY_MAX / 255
 
-viewer = o3d.visualization.Visualizer()
-viewer.create_window()
 
-for ann_file in os.listdir(ann_path):
-    fp = ann_path + ann_file
-    data_rows = np.genfromtxt(fp, delimiter=' ')
-    pcd_points = o3d.utility.Vector3dVector(data_rows[:,:3])
-    
-    if "pedestrian" in ann_file:
-        # Make all ped points red, and add a bbox around to indicate/distinguish the annotation
-        pcd_colors = o3d.utility.Vector3dVector([(1, 0, 0) for _ in range(len(data_rows))])
-        bbox = o3d.geometry.AxisAlignedBoundingBox.create_from_points(pcd_points)
-        lines = o3d.geometry.LineSet.create_from_axis_aligned_bounding_box(bbox)
-        viewer.add_geometry(lines)
-    elif "background" in ann_file:
-        # Make all points for background blue
-        pcd_colors = o3d.utility.Vector3dVector([(0, 0, 1) for _ in range(len(data_rows))])
+def viz_helper(frame_fd, draw_bbox):
 
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = pcd_points
-    pcd.colors = pcd_colors
+    ann_path = frame_fd + "Annotations/"
 
-    viewer.add_geometry(pcd)
+    viewer = o3d.visualization.Visualizer()
+    viewer.create_window()
 
-opt = viewer.get_render_option()
-opt.show_coordinate_frame = True
-opt.background_color = np.asarray([0.5, 0.5, 0.5])
-opt.point_size = 4
+    for ann_file in os.listdir(ann_path):
+        fp = ann_path + ann_file
+        data_rows = np.genfromtxt(fp, delimiter=' ')
 
-viewer.run()
-viewer.destroy_window()
+        # Commented out is code that colors with original intensity
+        # pcd_colors_arr = []
+        # for v in data_rows[:,3]:
+        #     intensity_val = int(min(v, INTENSITY_MAX) / COLOR_RANGE)
+        #     rgb = cm.jet(intensity_val)[:-1]
+        #     pcd_colors_arr.append(rgb)
+        # pcd_colors = o3d.utility.Vector3dVector(pcd_colors_arr)
+
+        pcd_points = o3d.utility.Vector3dVector(data_rows[:,:3])
+        pcd_colors = None
+        if "pedestrian" in ann_file:
+            # Make all ped points red, and add a bbox around to indicate/distinguish the annotation
+            pcd_colors = o3d.utility.Vector3dVector([(1, 0, 0) for _ in range(len(data_rows))])
+
+            if draw_bbox:
+                bbox = o3d.geometry.AxisAlignedBoundingBox.create_from_points(pcd_points)
+                lines = o3d.geometry.LineSet.create_from_axis_aligned_bounding_box(bbox)
+                viewer.add_geometry(lines)
+        elif "background" in ann_file:
+            # Make all points for background blue
+            pcd_colors = o3d.utility.Vector3dVector([(0, 0, 1) for _ in range(len(data_rows))])
+
+        else:
+            print("Skipping invalid annotation file name:", ann_file)
+            continue
+
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = pcd_points
+        pcd.colors = pcd_colors
+
+        viewer.add_geometry(pcd)
+
+    opt = viewer.get_render_option()
+    opt.show_coordinate_frame = True
+    opt.background_color = np.asarray([0.5, 0.5, 0.5])
+    opt.point_size = 3
+
+    viewer.run()
+    viewer.destroy_window()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='visualize annotations')
+    parser.add_argument('path', help='/abs/path/to/folder/frame_0000xxxx/', type=str)
+    parser.add_argument('-b','--bbox', help='Add bounding boxes on top of segmentation', action='store_true')
+
+    args = parser.parse_args()
+    frame_fd = args.path
+
+    viz_helper(frame_fd, args.bbox)
